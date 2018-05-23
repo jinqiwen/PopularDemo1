c194df205c4dc46ab9873dddb33176ca8dfd122c
jinqiwen <15256221809jin>
2018-05-23 23:02:43 +0800

    完善了git app 列表页面

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 
-import {View,Text,TextInput,StyleSheet} from 'react-native'
+import {View,Text,TextInput,StyleSheet,ListView,RefreshControl} from 'react-native'
 import NavigationBar from '../common/NavigationBar'
 import DataRepository from '../expand/dao/DataRepository'
+import RepositoryCell from '../common/RepositoryCell';
+import  ScrollableTabView,{ScrollableTabBar}  from 'react-native-scrollable-tab-view'
 const URL = 'https://api.github.com/search/repositories?q=';
 const QUERY_STR = '&sort=stars';
 export default  class PopularPage extends React.Component{
@@ -37,22 +39,83 @@ export default  class PopularPage extends React.Component{
         return(<View style={styles.container}>
             <NavigationBar
               title="最热"
-              style={{backgroundColor:'#6495ED'}}
+              statusBar={{
+               backgroundColor:'#2196F3'
+              }}
+
+             /* style={{backgroundColor:'#6495ED'}}*/
             />
-            <Text style={styles.tips}
-                  onPress={()=>{
-                      this.onLoad()
-                  }}
-            >获取数据</Text>
-            <TextInput
-                style={{height:40,borderWidth:1}}
-                onChangeText={text=>this.text=text}
-            />
-           <Text style={{height:500}}>{this.state.result}</Text>
+          <ScrollableTabView
+              tabBarBackgroundColor="#2196F3"
+              tabBarInactiveTextColor="mintcream"
+              tabBarActiveTextColor="white"
+              tabBarUnderlineStyle={{backgroundColor:"#e7e7e7",height:2}}
+             renderTabBar={()=><ScrollableTabBar/>}
+          >
+              <PopularTab tabLabel={"java"}>java</PopularTab>
+              <PopularTab tabLabel={"ios"}>ios</PopularTab>
+              <PopularTab tabLabel={"Android"}>Android</PopularTab>
+              <PopularTab tabLabel={"JavaScript"}>javaScript</PopularTab>
+          </ScrollableTabView>
         </View>)
     }
 }
+class PopularTab extends React.Component{
+    constructor(props) {
+        super(props);
+        this.dataRepository=new DataRepository()
+        this.state={
+            result:'',
+            dataSource:new ListView.DataSource({rowHasChanged:(r1,r2)=>r1==r2}),
+            isLoading:false
+        }
+    }
 
+    componentDidMount() {
+        this.loadData()
+    }
+    loadData(){
+        this.setState({isLoading:true});
+        let url=URL+this.props.tabLabel+QUERY_STR;
+        this.dataRepository.fetchNetRepository(url)
+            .then(result=>{
+               /* alert(result);*/
+                this.setState({
+                    dataSource :this.state.dataSource.cloneWithRows(result.items),
+                     isLoading:false
+                })
+            })
+            .catch(error=>{
+                this.setState({
+                    result:JSON.stringify(error)
+                })
+            })
+
+    }
+    /*返回每一行的结构内容*/
+    renderRow(data){
+        return <RepositoryCell data={data}/>
+    }
+    render() {
+        return(<View style={{flex:1}}>
+       <ListView
+           dataSource={this.state.dataSource}
+           renderRow={(data)=>this.renderRow(data)}
+               refreshControl={
+                   <RefreshControl
+                       refreshing={this.state.isLoading}
+                       onRefresh={()=>this.loadData()}
+                       colors={['#2196F3']}
+                       tintColor={'#2196F3'}
+                       title={'Loading...'}
+                   />
+               }
+
+       />
+
+          </View>)
+    }
+}
 const  styles=StyleSheet.create({
     tips:{
         fontSize:29
