985c33c5839c25efcc2c80b0a246f3918be51a61
jinqiwen <15256221809jin>
2018-05-24 22:30:50 +0800

    实现自定义标签+标签动态修改+首页动态更新+底部导航

---
+++ js/expand/dao/LanguageDao.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+ AsyncStorage
+} from 'react-native';
+import keys from '../../../res/data/keys.json'
+export var FLAG_LANGUAGE={flag_language:'language_dao_language',flag_key:'language_dao_key' };
+export default  class LanguageDao {
+    constructor(flag){
+        this.flag=flag;
+
+    }
+
+      fetch(){
+          return new Promise((resolve,reject)=> {
+              AsyncStorage.getItem(this.flag, (error, result) => {
+                  if (error) {
+                      reject(error);
+                      return;
+                  } else {
+                      if (result) {
+                          try {
+                              resolve(JSON.parse(result))
+                          } catch (e) {
+                              reject(e);
+                          }
+                      } else {
+                          var data = this.flag === FLAG_LANGUAGE.flag_key? keys : null;
+                          this.save(data);
+                          resolve(JSON.parse(data));
+
+                      }
+
+                  }
+              })
+          })
+      }
+              save(data){
+               AsyncStorage.setItem(this.flag,JSON.stringify(data),(error,result)=>{
+
+               })
+              }
+
+
+   }
